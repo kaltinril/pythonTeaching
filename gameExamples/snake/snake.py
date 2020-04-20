52e184692bd4fc7a2a28a84e1b279a1ede1d6332
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2020-04-19 19:25:18 -0800

    Add fruit!

--- gameExamples/snake/snake.py
+++ gameExamples/snake/snake.py
@@ -2,17 +2,18 @@
 import sys
 import pygame
 from pygame.locals import *
+import random
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 fps = 60.0
 
 # List of points for the snake
 snake = [(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)]
-xv = 0
-yv = 0
+snake_v = (0, 0)
 snake_size = 2
 
 # List of "fruit" to eat
@@ -22,6 +23,7 @@ fruit = [(50, 50)]
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 fps_clock = pygame.time.Clock()
+time_elapsed_since_last_action = 0
 
 # loop
 while True:
@@ -32,21 +34,21 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and yv == 0:
-                yv = -1
-                xv = 0
-            elif event.key == pygame.K_DOWN and yv == 0:
-                yv = 1
-                xv = 0
-            elif event.key == pygame.K_LEFT and xv == 0:
-                yv = 0
-                xv = -1
-            elif event.key == pygame.K_RIGHT and xv == 0:
-                yv = 0
-                xv = 1
+            if event.key == pygame.K_UP and snake_v[1] == 0:
+                snake_v = (0, -1)
+            elif event.key == pygame.K_DOWN and snake_v[1] == 0:
+                snake_v = (0, 1)
+            elif event.key == pygame.K_LEFT and snake_v[0] == 0:
+                snake_v = (-1, 0)
+            elif event.key == pygame.K_RIGHT and snake_v[0] == 0:
+                snake_v = (1, 0)
 
     # Clear screen
     screen.fill(BLACK)  # Fill the screen with black.
+
+    # Draw the fruit
+    for f in fruit:
+        pygame.draw.circle(screen, GREEN, f, 2)
 
     # draw snake
     for point in snake:
@@ -56,7 +58,7 @@ while True:
 
     # move snake by adding another point with the velociy/direction, to the last position and adding it to the list
     last_point = snake[-1]
-    new_point = (last_point[0] + (xv * snake_size), last_point[1] + (yv* snake_size))
+    new_point = (last_point[0] + (snake_v[0] * snake_size), last_point[1] + (snake_v[1] * snake_size))
     snake.append(new_point)
 
     # remove the last point
@@ -74,7 +76,15 @@ while True:
         xv = 0
         yv = 0
 
+    # Move the fruit every 10 seconds
+    if time_elapsed_since_last_action > 10000:
+        x = random.randint(10, SCREEN_WIDTH - 10)
+        y = random.randint(10, SCREEN_HEIGHT - 10)
+        fruit.pop()
+        fruit.append((x, y))
+        time_elapsed_since_last_action = 0
 
     # Force to 60 FPS
-    fps_clock.tick(fps)
+    dt = fps_clock.tick(fps)
+    time_elapsed_since_last_action += dt
 
